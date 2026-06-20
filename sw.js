c522c187af8e47fcd7780abb9adc010a0772b21f
LucasCunh4 <Lucas260802@gmail.com>
2026-06-20 16:34:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_BUSTER = 'stowage-plan-cachefix-2026-06-20-v5';
+const CACHE_BUSTER = 'stowage-plan-cachefix-2026-06-20-v6';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
