c21341490df857ca970eac49dce9d375764744da
LucasCunh4 <Lucas260802@gmail.com>
2026-06-22 17:11:35 -0300

    Update CACHE_BUSTER version to v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_BUSTER = 'stowage-plan-cachefix-2026-06-20-v8';
+const CACHE_BUSTER = 'stowage-plan-cachefix-2026-06-20-v9';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
